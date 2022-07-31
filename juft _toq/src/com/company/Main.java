14efc63b1b4481e64b4c723ff52abeaecd1563cb
Humoyun26 <110013343+Humoyun26@users.noreply.github.com>
2022-07-31 03:46:54 -0400

    Add files via upload

---
+++ juft _toq/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner =new Scanner(in);
+
+        System.out.println("son kiriting:  ");
+        int n = scanner.nextInt();
+
+
+        if (n % 2 == 0) {
+            System.out.println("juft");
+        }
+
+        if (n % 2 != 0) {
+            System.out.println("toq");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
